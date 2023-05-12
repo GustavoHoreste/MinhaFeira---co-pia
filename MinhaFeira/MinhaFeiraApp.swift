824ad832a82797ef580f45b3e78f24489fd72adb
Gustavo <gustavohoreste@gmail.com>
2023-05-12 17:24:15 -0300

    Initial Commit

---
+++ MinhaFeira/MinhaFeiraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinhaFeiraApp.swift
+//  MinhaFeira
+//
+//  Created by Gustavo Horestee Santos Barros on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MinhaFeiraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
